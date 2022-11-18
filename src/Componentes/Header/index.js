b4e4eb3a757497f223741ec8f1f14fd36ef88b82
92nato <nato6661992@gmail.com>
2022-11-18 15:09:10 -0300

    makin a header for the storage App

---
+++ src/Componentes/Header/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import logo from '../../images/logo.jfif'
+
+
+export const Header =() => {
+    return (
+        <hedaer>
+              
+                <a href='#'>
+                    <div className='logo'>
+                        <img src={logo} alt=''></img>
+                    </div>
+                </a>
+
+                <ul>
+                    <li>
+                        <a href='#'>INICIO</a>
+                    </li>
+                    <li>
+                        <a href='#'>PRODUCTOS</a>
+                    </li>
+                </ul>
+                <box-icon name="cart"></box-icon>
+                <span className='item__total'>0</span>
+        </hedaer>
+    )
+}
